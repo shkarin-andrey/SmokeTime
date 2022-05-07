cdcaef2f27d11e82a6a07335db9361c773c058e4
Andrey Shkarin <shkarinandrew@mail.ru>
2022-05-07 19:32:54 +0300

    Fix accordion

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import ArrowDown from "../../public/icons/arrowDown";
+import AccordionItem from "./AccordionItem";
 
 interface iAccordion {
   accordionList: iAccordionItem[];
@@ -11,33 +11,10 @@ interface iAccordionItem {
 }
 
 const Accordion: FC<iAccordion> = ({ accordionList }) => {
-  const [show, setShow] = useState(0);
   return (
     <div className="accordion">
       {accordionList.map((item, i) => (
-        <div
-          key={i}
-          className={`accordion__item ${show === i ? "active" : ""}`}
-        >
-          <div
-            className={`accordion__item__top ${show === i ? "border-1" : ""}`}
-            onClick={() => setShow(i)}
-          >
-            <h3 className="accordion__item__top-title">{item.title}</h3>
-            <div
-              className={`accordion__item__top-button ${
-                show !== i ? "rotate-0" : "rotate-180"
-              }`}
-            >
-              <ArrowDown />
-            </div>
-          </div>
-          <div
-            className={`accordion__item__bottom ${show !== i ? "hidden" : ""}`}
-          >
-            <div className="accordion__item__bottom-descr">{item.descr}</div>
-          </div>
-        </div>
+        <AccordionItem key={i} title={item.title} descr={item.descr} />
       ))}
     </div>
   );
---
+++ components/Accordion/AccordionItem.tsx
@@ -0,0 +1,37 @@
+import { FC, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import ArrowDown from "../../public/icons/arrowDown";
+
+interface iAccordionItem {
+  title: string;
+  descr: string;
+}
+
+const AccordionItem: FC<iAccordionItem> = ({ title, descr }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className={`accordion__item`}>
+      <div className={`accordion__item__top`} onClick={() => setShow(!show)}>
+        <h3 className="accordion__item__top-title">{title}</h3>
+        <div
+          className={`accordion__item__top-button ${
+            !show ? "rotate-0" : "rotate-180"
+          }`}
+        >
+          <ArrowDown />
+        </div>
+      </div>
+      <CSSTransition
+        in={show}
+        timeout={300}
+        classNames="accordion__item__bottom"
+      >
+        <div className={`accordion__item__bottom`}>
+          <div className="accordion__item__bottom-descr">{descr}</div>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default AccordionItem;
